3b6b4d83bb42299ef8a872be60ad161359ae4377
Thiago de Carvalho <thiagovowclash@gmail.com>
2024-11-02 00:16:09 -0300

    first commit

---
+++ src/const.js
@@ -0,0 +1,12 @@
+export const scaleFactor = 4;
+
+export const dialogueData = {
+    pc: `Sorry. This is not a real PC :/`,
+    "cs-degree": `Not to see here :o`,
+    "sofa-table": `That's comfortable.`,
+    tv: `Watching TV all day makes my brain go bad.`,
+    bed: `I never sleep here. I prefer the floor.`,
+    projects: `I did everything. I'm really fast.`,
+    library: `Peppa pig books`,
+    exit: `Don't leave, pleaseeee`,
+  };
